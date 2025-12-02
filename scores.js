16f60a12457ee1c86821a7b71a520f2a43128a3d
Heroaran <heroaran50@gmail.com>
2025-12-02 23:31:47 +0900

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,1 +1,525 @@
-
+const scores = [
+      {
+        title: "카트라이더 배찌 뒹굴뒹굴 (빌리지 bgm)",
+        composer: "NEXON",
+        instrument: "바이올린, 피아노",
+        difficulty: "6화음",
+        tags: ["카트라이더", "빌리지", "넥슨"],
+        url: "https://gall.dcinside.com/enban/199030",
+        solo: true
+      },
+      {
+        title: "HOI4 - Battle of Wuhan",
+        composer: "패러독스사",
+        instrument: "피아노",
+        difficulty: "4화음",
+        tags: ["호이", "하츠오브아이언", "우한", "우한전투"],
+        url: "https://gall.dcinside.com/enban/260356",
+        solo: true
+      },
+      {
+        title: "Not So Far Away",
+        composer: "라그나로크 온라인",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["라그나로크 온라인", "게임", "BGM"],
+        url: "https://gall.dcinside.com/enban/687",
+        solo: true
+      },
+      {
+        title: "난 좋아",
+        composer: "성시경",
+        instrument: "피아노",
+        difficulty: "6화음",
+        tags: ["성시경", "발라드", "KPOP"],
+        url: "https://gall.dcinside.com/enban/1106",
+        solo: true
+      },
+      {
+        title: "귀멸의 칼날 무한성 브금",
+        composer: "귀멸의 칼날",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["귀멸의칼날", "귀칼", "무한성"],
+        url: "https://gall.dcinside.com/enban/3475",
+        solo: true
+      },
+      {
+        title: "Happiness Bell",
+        composer: "a hisa",
+        instrument: "류트, 실로폰, 바이올린, 플룻",
+        difficulty: "6화음",
+        tags: ["해피니스 벨", "히사"],
+        url: "https://gall.dcinside.com/enban/3885",
+        solo: true
+      },
+      {
+        title: "따잇하는 재미",
+        composer: "메벤남",
+        instrument: "만돌린, 샬루모, 바이올린, 플루트",
+        difficulty: "4화음",
+        tags: ["창팝", "메이플", "신창섭", "메벤남", "밈"],
+        url: "https://gall.dcinside.com/enban/4125",
+        solo: true
+      },
+      {
+        title: "String Theocracy(실의 신권정치)",
+        composer: "Mili",
+        instrument: "피아노, 실로폰, 샬루모, 류트",
+        difficulty: "6화음",
+        tags: ["라이브러리 오브 루이나", "OST"],
+        url: "https://gall.dcinside.com/enban/4373",
+        solo: true
+      },
+      {
+        title: "Attack on Titan",
+        composer: "진격의거인",
+        instrument: "만돌린, 바이올린",
+        difficulty: "4화음",
+        tags: ["어택온티탄", "진격의거인", "진격거", "OST", "애니"],
+        url: "https://gall.dcinside.com/enban/25381",
+        solo: true
+      },
+      {
+        title: "Vogel im Kafig(새장속의 새)",
+        composer: "진격의거인",
+        instrument: "만돌린",
+        difficulty: "3화음",
+        tags: ["어택온티탄", "진격의거인", "진격거", "OST", "애니"],
+        url: "https://gall.dcinside.com/enban/25345",
+        solo: true
+      },
+      {
+        title: "TICKING AWAY",
+        composer: "라이엇 게임즈",
+        instrument: "만돌린, 피아노",
+        difficulty: "3화음",
+        tags: ["발로란트", "라이엇", "티킹어웨이"],
+        url: "https://gall.dcinside.com/enban/25335",
+        solo: true
+      },
+      {
+        title: "반야심경 뽕짝 리믹스",
+        composer: "안치행",
+        instrument: "만돌린, 샬루모",
+        difficulty: "3화음",
+        tags: ["반야심경", "불교", "리믹스", "힙합", "밈"],
+        url: "https://gall.dcinside.com/enban/389528",
+        solo: true
+      },
+      {
+        title: "Old Mad Marionette Piano Party",
+        composer: "Plum",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["피아노곡", "클래식", "리듬게임", "Plum"],
+        url: "https://gall.dcinside.com/enban/381462",
+        solo: true
+      },
+      {
+        title: "과거의 물결 - 붕괴 스타레일「안녕, 세상아!」삽입곡",
+        composer: "HoYoverse",
+        instrument: "피아노",
+        difficulty: "6화음",
+        tags: ["호요버스", "붕괴", "게임"],
+        url: "https://gall.dcinside.com/enban/346311",
+        solo: false
+      },
+      {
+        title: "바람이 되어 - 고양이의 보은 OST",
+        composer: "츠지 아야노",
+        instrument: "만돌린",
+        difficulty: "3화음",
+        tags: ["고양이의 보은", "JPOP", "OST"],
+        url: "https://gall.dcinside.com/enban/337534",
+        solo: true
+      },
+      {
+        title: "뒷골목 사탕가게",
+        composer: "Plum",
+        instrument: "피아노, 실로폰, 플룻, 바이올린",
+        difficulty: "6화음",
+        tags: ["클래식", "리듬게임", "Plum"],
+        url: "https://gall.dcinside.com/enban/328087",
+        solo: false
+      },
+      {
+        title: "Fly Kid Fly",
+        composer: "Plum",
+        instrument: "피아노, 실로폰, 플룻, 바이올린",
+        difficulty: "3화음",
+        tags: ["클래식", "리듬게임", "Plum"],
+        url: "https://gall.dcinside.com/enban/294314",
+        solo: true
+      },
+      {
+        title: "카트라이더 상점 BGM",
+        composer: "NEXON",
+        instrument: "피아노",
+        difficulty: "2화음",
+        tags: ["카트라이더", "게임", "상점", "넥슨"],
+        url: "https://gall.dcinside.com/enban/189955",
+        solo: true
+      },
+      {
+        title: "판관 포청천 주제곡",
+        composer: "미상",
+        instrument: "만돌린, 샬루모, 바이올린, 플루트",
+        difficulty: "4화음",
+        tags: ["포청천", "OST", "드라마", "밈"],
+        url: "https://gall.dcinside.com/enban/156831",
+        solo: true
+      },
+      {
+        title: "트리 오브 세이비어 - Charty of fire",
+        composer: "NEXON",
+        instrument: "피아노, 바이올린, 만돌린, 큰북",
+        difficulty: "12화음",
+        tags: ["트오세", "넥슨", "게임"],
+        url: "https://gall.dcinside.com/enban/155305",
+        solo: false
+      },
+       {
+        title: "모비노기 미스틱 던전 BGM",
+        composer: "데브캣",
+        instrument: "만돌린, 바이올린, 플루트, 샬루모",
+        difficulty: "4화음",
+        tags: ["마비노기 모바일", "미스틱 던전", "모비노기"],
+        url: "https://gall.dcinside.com/enban/126028",
+        solo: true
+      },
+      {
+        title: "시간의 강을 거슬러",
+        composer: "Plum",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["클래식", "리듬게임", "Plum"],
+        url: "https://gall.dcinside.com/enban/100866",
+        solo: true
+      },
+      {
+        title: "모비노기 보스 처형곡 미래를 손에 쥐다(Raid ver.)",
+        composer: "데브캣",
+        instrument: "만돌린, 큰북, 바이올린, 샬루모",
+        difficulty: "4화음",
+        tags: ["마비노기 모바일", "레이드", "모비노기"],
+        url: "https://gall.dcinside.com/enban/93530",
+        solo: true
+      },
+      {
+        title: "아씨 걸음",
+        composer: "Neal K",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["클래식", "리듬게임", "Neal K"],
+        url: "https://gall.dcinside.com/enban/84090",
+        solo: true
+      },
+      {
+        title: "글라스기브넨 최종무곡 (글라스기브넨)",
+        composer: "데브캣",
+        instrument: "만돌린, 바이올린, 플루트, 샬루모",
+        difficulty: "4화음",
+        tags: ["마비노기 모바일", "레이드", "모비노기", "글라스기브넨", "마비노기"],
+        url: "https://gall.dcinside.com/enban/83421",
+        solo: true
+      },
+      {
+        title: "Milky Way Hitchhiker",
+        composer: "Plum",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["클래식", "리듬게임", "Plum"],
+        url: "https://gall.dcinside.com/enban/29819",
+        solo: true
+      },
+      {
+        title: "이기적인 놈, 메이플이 망해도 좋다. 이거냐?!",
+        composer: "메벤남",
+        instrument: "피아노",
+        difficulty: "3화음",
+        tags: ["창팝", "메이플", "신창섭", "메벤남", "밈"],
+        url: "https://gall.dcinside.com/enban/20316",
+        solo: true
+      },
+      {
+        title: "호접몽",
+        composer: "Neal K",
+        instrument: "피아노, 바이올린, 실로폰, 플루트",
+        difficulty: "6화음",
+        tags: ["클래식", "리듬게임", "Neal K"],
+        url: "https://gall.dcinside.com/enban/17995",
+        solo: false
+      },
+      {
+        title: "행렬이 이어지는 에린진료소",
+        composer: "동방프로젝트",
+        instrument: "바이올린, 피아노",
+        difficulty: "4화음",
+        tags: ["동방", "게임"],
+        url: "https://gall.dcinside.com/enban/475533",
+        solo: true
+      },
+      {
+  title: "녹색전차해모수 오프닝",
+  composer: "녹색전차해모수",
+  instrument: "피아노, 만돌린",
+  difficulty: "3화음",
+  tags: ["녹색전차해모수"],
+  url: "https://gall.dcinside.com/m/enban/401927",
+  solo: true
+},
+{
+  title: "엘더스크롤 스카이림 - The Dragonborn comes",
+  composer: "엘더스크롤 V: 스카이림",
+  instrument: "피아노",
+  difficulty: "4화음",
+  tags: ["엘더스크롤", "스카이림", "독주가능", "게임 ost"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=enban&no=475251&search_head=40&page=1",
+  solo: true
+},
+{
+  title: "메이플스토리 - 루디브리엄 가는 길 (Flying in a blue dream)",
+  composer: "NEXON",
+  instrument: "피아노",
+  difficulty: "4화음",
+  tags: ["메이플스토리", "합주추천", "넥슨"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1067336&page=1",
+  solo: true
+},
+{
+  title: "메멘토모리 아모르테마곡 Etoile",
+  composer: "메멘토 모리: Memento Mori",
+  instrument: "류트, 만돌린, 피아노",
+  difficulty: "6화음",
+  tags: ["메멘토 모리", "일본 모바일게임", "독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=801133&search_head=150&page=1",
+  solo: true
+},
+{
+  title: "붕괴스타레일 히아킨 PV 뭐든 도와드릴께요",
+  composer: "붕괴: 스타레일",
+  instrument: "만돌린, 류트, 피아노",
+  difficulty: "6화음",
+  tags: ["호요버스", "붕괴스타레일", "미호요", "합주추천", "독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1118296&search_head=150&page=1",
+  solo: true
+},
+{
+  title: "붕괴 스타레일 - 응결된 파도",
+  composer: "붕괴: 스타레일",
+  instrument: "실로폰, 피아노",
+  difficulty: "5화음",
+  tags: ["붕괴스타레일", "호요버스", "미호요", "합주추천"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=enban&no=470824&page=1",
+  solo: true
+},
+{
+  title: "붕괴 스타레일 - 마음의 기억",
+  composer: "붕괴: 스타레일",
+  instrument: "실로폰, 피아노",
+  difficulty: "4화음",
+  tags: ["호요버스", "붕괴스타레일", "미호요", "3화음악기독주가능", "합주추천"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=enban&no=398177&page=1",
+  solo: true
+},
+{
+  title: "붕괴스타레일 키레네PV - 다시 너와 (With You Once More)",
+  composer: "붕괴: 스타레일",
+  instrument: "플루트, 피아노",
+  difficulty: "6화음",
+  tags: ["붕괴스타레일", "호요버스", "미호요", "합주추천"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=enban&no=393416&page=1",
+  solo: true
+},
+{
+  title: "붕괴스타레일 로빈 곡 - 은하에서 외로이 흔들리며",
+  composer: "붕괴: 스타레일",
+  instrument: "실로폰, 류트, 피아노",
+  difficulty: "5화음",
+  tags: ["붕괴스타레일", "호요버스", "미호요", "합주추천", "3화음악기독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=771379",
+  solo: true
+},
+{
+  title: "붕괴스타레일 - 히든필드 기억속의 꽃바다",
+  composer: "붕괴: 스타레일",
+  instrument: "실로폰, 피아노",
+  difficulty: "6화음",
+  tags: ["붕괴스타레일", "호요버스", "미호요", "실로폰합주추천", "피아노독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=786455",
+  solo: true
+},
+{
+  title: "붕괴스타레일 - Nameless Faces",
+  composer: "붕괴: 스타레일",
+  instrument: "피아노",
+  difficulty: "6화음",
+  tags: ["붕괴스타레일", "호요버스", "미호요", "합주추천", "4화음이상"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=995579",
+  solo: false
+},
+{
+  title: "가끔씩 툭하고 러시아어로 부끄러워하는 옆자리의 아랴 양 OP",
+  composer: "우에사카 스미레",
+  instrument: "피아노",
+  difficulty: "5화음",
+  tags: ["로시데레", "애니메이션", "오프닝", "독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1170063&search_head=150&page=1",
+  solo: true
+},
+{
+  title: "메이플스토리 - 세르니움 BGM - The Holy Land (실로폰)",
+  composer: "NEXON",
+  instrument: "실로폰, 피아노",
+  difficulty: "6화음",
+  tags: ["메이플스토리", "6실로폰합주추천", "넥슨", "피아노독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=736278",
+  solo: true
+},
+      {
+  title: "메이플스토리 샤레니안의 기사 - My Princes, My Kingdom",
+  composer: "NEXON",
+  instrument: "류트, 피아노",
+  difficulty: "6화음",
+  tags: ["메이플스토리", "독주가능", "넥슨"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=783696",
+  solo: true
+},
+{
+  title: "메이플스토리 - 세레니티",
+  composer: "NEXON",
+  instrument: "류트, 피아노",
+  difficulty: "6화음",
+  tags: ["메이플스토리", "합주추천", "넥슨"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1082938&page=1",
+  solo: false
+},
+{
+  title: "마비노기 시드피나하 BGM - Shadows in the water",
+  composer: "데브캣",
+  instrument: "실로폰, 피아노",
+  difficulty: "5화음",
+  tags: ["마비노기", "5실로폰합주추천", "넥슨"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=818491",
+  solo: false
+},
+{
+  title: "사계 1989",
+  composer: "노래를 찾는 사람들",
+  instrument: "류트, 피아노",
+  difficulty: "4화음",
+  tags: ["민중가요", "독주가능", "노찾사"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1622513&page=1",
+  solo: true
+},
+{
+  title: "걸즈 앤 판처 극장판 ost - 학원십색입니다!",
+  composer: "걸즈 앤 판처",
+  instrument: "류트, 만돌린, 피아노",
+  difficulty: "6화음",
+  tags: ["애니메이션", "메들리", "해외군가", "합주추천"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=672333",
+  solo: true
+},
+{
+  title: "양파의노래 - 걸즈 앤 판처ver",
+  composer: "걸즈 앤 판처",
+  instrument: "피아노",
+  difficulty: "6화음",
+  tags: ["애니메이션ver", "해외군가", "독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=enban&no=393487&search_head=40&page=1",
+  solo: true
+},
+{
+  title: "작안의샤나 2기 ED - sociometry",
+  composer: "KOTOKO",
+  instrument: "류트, 피아노",
+  difficulty: "6화음",
+  tags: ["작안의샤나", "애니메이션", "샤나", "독주가능"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1284031&search_head=150&page=1",
+  solo: true
+},
+{
+  title: "Summer pockets - 츠무기의 여름방학",
+  composer: "이와이 에미리",
+  instrument: "피아노",
+  difficulty: "6화음",
+  tags: ["독주불가", "6화음합주", "애니메이션", "비주얼아츠", "서머포켓츠", "무규규"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1253729&page=1",
+  solo: false
+},
+{
+  title: "판도라하츠 Everytime you kissed me",
+  composer: "카지우라 유키",
+  instrument: "실로폰, 피아노",
+  difficulty: "6화음",
+  tags: ["피아노독주가능", "실로폰합주추천", "애니메이션", "판도라하츠"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1195323&search_head=150&page=1",
+  solo: true
+},
+ {
+  title: "스카보로 페어 - 애니메이션 종말에뭐하세요? 바쁘세요? 구해주실 수 있나요?",
+  composer: "야마다 타마루",
+  instrument: "바이올린, 플루트, 실로폰, 류트, 피아노",
+  difficulty: "5화음",
+  tags: ["애니메이션ver", "종말에뭐하세요?", "여러악기합주추천"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1112355&page=1",
+  solo: false
+},
+      {
+  title: "종말에 뭐 하세요? 바쁘세요? 구해 주실 수 있나요? Always in my heart",
+  composer: "야마다 타마루",
+  instrument: "플루트, 피아노, 실로폰, 류트",
+  difficulty: "6화음",
+  tags: ["종말에뭐하세요?시리즈", "독주불가", "여러악기합주추천", "애니메이션"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1111642&search_head=150&page=1",
+  solo: false
+},
+{
+  title: "리틀버스터즈 Song for Friends",
+  composer: "리타",
+  instrument: "피아노",
+  difficulty: "5화음",
+  tags: ["리틀버스터즈", "비주얼아츠", "독주가능", "합주추천", "애니메이션"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1778412&page=1",
+  solo: true
+},
+{
+  title: "ATRI - My Dear Moments OP - 히카리하나테!",
+  composer: "야나기 마미",
+  instrument: "피아노",
+  difficulty: "6화음",
+  tags: ["독주불가", "6화음합주", "노벨게임", "아트리", "애니메이션"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=1844395",
+  solo: false
+},
+{
+  title: "마비노기 어릴적 할머니가 들려주신 옛 전설 (월몽 버전)",
+  composer: "데브캣",
+  instrument: "실로폰, 류트, 피아노",
+  difficulty: "3화음",
+  tags: ["마비노기", "독주가능", "넥슨", "할로윈"],
+  url: "https://gall.dcinside.com/mgallery/board/view/?id=mabinogimobile&no=1179525&page=1",
+  solo: true
+},
+{
+  title: "테일즈 오브 판타지아 - The Second Act",
+  composer: "테일즈 오브 판타지아",
+  instrument: "샬루모, 실로폰, 류트, 피아노, 만돌린",
+  difficulty: "4화음",
+  tags: ["여러악기합주추천", "반다이남코", "테일즈오브시리즈", "플레이스테이션", "일본게임"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=832030",
+  solo: true
+},
+{
+  title: "임형주 - 천개의 바람이 되어",
+  composer: "임형주",
+  instrument: "류트, 피아노",
+  difficulty: "5화음",
+  tags: ["장송곡", "독주가능", "임형주"],
+  url: "https://gall.dcinside.com/mgallery/board/view?id=mabinogimobile&no=879820",
+  solo: true
+}
+      // 여기에 다른 악보도 계속 추가
+    ];
